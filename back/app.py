194ce18e19313ea2e93d2588bf8f2e6b89f124ac
seonxx <71209456+seung-eon@users.noreply.github.com>
2024-03-31 22:18:49 +0900

    ADD: attentance get api

--- back/app.py
+++ back/app.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, session, jsonify
-from datetime import datetime, timedelta, timezone
+from datetime import datetime
 import pymysql.cursors
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'
+app.secret_key = '34c9fff6c54c731441fddb33548aee32c0ec8faaf7e38563'
 
 db = pymysql.connect(host='127.0.0.1',
                        user='root',
@@ -16,6 +16,7 @@ db = pymysql.connect(host='127.0.0.1',
 def home():
     return 'This is home!'
 
+# 로그인
 @app.route("/login", methods=["POST"])
 def login():
     if request.method == 'POST':
@@ -28,13 +29,14 @@ def login():
             cursor.execute(sql, (name, password))
             user = cursor.fetchone()
 
-        if user: # 로그인 성공
+        if user:
             session['user_id'] = user['id']
             session['username'] = user['name']
             return jsonify({'message': '로그인 성공', 'user': user}), 200
         else:
             return jsonify({'message': '아이디 또는 비밀번호가 잘못되었습니다.'}), 401
-    
+
+# 로그아웃    
 @app.route("/logout",  methods=['GET'])
 def logout():
     # 세션에서 사용자 정보 삭제
@@ -42,7 +44,8 @@ def logout():
     session.pop('username', None)
     
     return jsonify({'message': '로그아웃 되었습니다.'}), 200
-    
+
+# 출석 등록
 @app.route("/checkin", methods=["POST"])
 def checkin():
     if 'user_id' in session:
@@ -50,7 +53,7 @@ def checkin():
         date = datetime.now().date()
         start_time = datetime.now()
 
-        # 출석 데이터 추가
+        # 로그인 상태에서만 출석 데이터 추가
         with db.cursor() as cursor:
             sql = "INSERT INTO attendance (date, start_time, name) VALUES (%s, %s, %s)"
             cursor.execute(sql, (date, start_time, name))
@@ -59,6 +62,7 @@ def checkin():
     else:
         return jsonify({'message': '로그인이 필요합니다.'}), 401
 
+# 퇴근 등록
 @app.route("/checkout", methods=["POST"])
 def checkout():
     if 'user_id' in session:
@@ -83,8 +87,87 @@ def checkout():
     else:
         return jsonify({'message': '로그인이 필요합니다.'}), 401
 
-#@app.route('/attendance', methods=['GET'])
-#def get_attendance():
+# 전체 출결 카운트 조회
+@app.route('/attendance', methods=['GET'])
+def get_attendance():
+    if 'user_id' in session:
+        # 사용자의 이름을 세션 ID를 사용하여 가져옴
+        user_id = session['user_id']
+        with db.cursor() as cursor:
+            sql = "SELECT name FROM users WHERE id = %s"
+            cursor.execute(sql, (user_id,))
+            user = cursor.fetchone()
+        
+        if user:
+            name = user['name']
+
+            # 로그인한 사용자의 출결 조회 쿼리 실행
+            with db.cursor() as cursor:
+                sql = """
+                    SELECT 
+                        SUM(start_time > CAST(CONCAT(date, ' 09:10:00') AS DATETIME)) AS 지각,
+                        SUM(start_time <= CAST(CONCAT(date, ' 09:10:00') AS DATETIME) AND end_time < CAST(CONCAT(date, ' 17:50:00') AS DATETIME)) AS 조퇴,
+                        SUM(start_time <= CAST(CONCAT(date, ' 09:10:00') AS DATETIME) AND end_time >= CAST(CONCAT(date, ' 17:50:00') AS DATETIME)) AS 출석,
+                        SUM(start_time IS NULL OR end_time IS NULL OR TIMESTAMPDIFF(MINUTE, start_time, end_time) < 240) AS 결석
+                    FROM attendance
+                    WHERE name = %s;
+                """
+                cursor.execute(sql, (name,))
+                result = cursor.fetchone()
+
+            # 결과가 있으면 JSON 형태로 반환
+            if result:
+                return jsonify(result), 200
+            else:
+                return jsonify({'지각': 0, '조퇴': 0, '출석': 0, '결석': 0}), 404
+        else:
+            return jsonify({'message': '사용자를 찾을 수 없습니다.'}), 404
+    else:
+        return jsonify({'message': '로그인이 필요합니다.'}), 401
+
+# 일자별 출결 조회
+@app.route('/attendance/date', methods=['POST'])
+def get_attendance_by_date():
+    if 'user_id' in session:
+        user_id = session['user_id']
+        
+        # 조회할 날짜
+        date = request.json.get('date')
+
+        # 사용자의 이름을 세션 ID를 사용하여 가져옴
+        with db.cursor() as cursor:
+            sql = "SELECT name FROM users WHERE id = %s"
+            cursor.execute(sql, (user_id,))
+            user = cursor.fetchone()
+        
+        if user:
+            name = user['name']
+
+            # 출결 조회 쿼리 실행
+            with db.cursor() as cursor:
+                sql = """
+                    SELECT 
+                        SUM(start_time > CAST(CONCAT(%s, ' 09:10:00') AS DATETIME)) AS 지각,
+                        SUM(start_time <= CAST(CONCAT(%s, ' 09:10:00') AS DATETIME) AND end_time < CAST(CONCAT(%s, ' 17:50:00') AS DATETIME)) AS 조퇴,
+                        SUM(start_time <= CAST(CONCAT(%s, ' 09:10:00') AS DATETIME) AND end_time >= CAST(CONCAT(%s, ' 17:50:00') AS DATETIME)) AS 출석,
+                        SUM(start_time IS NULL OR end_time IS NULL OR TIMESTAMPDIFF(MINUTE, start_time, end_time) < 240) AS 결석,
+                        start_time AS 출석시간,
+                        end_time AS 퇴실시간
+                    FROM attendance
+                    WHERE name = %s AND date = %s
+                """
+                cursor.execute(sql, (date, date, date, date, date, name, date))
+                result = cursor.fetchone()
+
+            # 결과가 있으면 JSON 형태로 반환
+            if result:
+                return jsonify(result), 200
+            else:
+                return jsonify({'message': '해당 날짜에 대한 출결 기록이 없습니다.'}), 404
+        else:
+            return jsonify({'message': '사용자를 찾을 수 없습니다.'}), 404
+    else:
+        return jsonify({'message': '로그인이 필요합니다.'}), 401
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
