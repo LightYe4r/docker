b0792c0b9c7cf4a2dc379ef44bba613b2dc23bd5
kijungle <kijung982@gmail.com>
2024-04-01 12:47:18 +0900

    [FIX] Database Schema

--- db/initdb.d/1.create_attendance.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE attendance (
-    id INT PRIMARY KEY,
-    date DATE NOT NULL,
-    start_time DATETIME NOT NULL,
-    end_time DATETIME,
-    name VARCHAR(255) NOT NULL
-);
---
+++ db/initdb.d/1.create_table.sql
@@ -0,0 +1,14 @@
+create table users (
+   id int not null, 
+   name varchar(255) primary key, 
+   password varchar(255) not null
+);
+
+create table attendance (
+   id int primary key,
+   date DATE not null,
+   start_time datetime default null,
+   end_time datetime default null,
+   name varchar(255) not null,
+   FOREIGN KEY (name) REFERENCES users(name)
+);
