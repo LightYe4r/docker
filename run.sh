4c513899979c7865fa72b3963c859c5c91e4fe84
kijungle <kijung982@gmail.com>
2024-04-03 21:46:40 +0900

    [FIX] run.sh & docker-compose.yaml

--- run.sh
+++ run.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-database-image=
-backend-image=
-haproxy-image=
-frontend-image=
-nginx-image=
+front_net="frontend"
+back_net="backend"
+database_image="kijung2/docker-mysql:latest"
+backend_image="docker-flask:latest"
+haproxy_image="haproxy-test:1.0"
+frontend_image="frontend-test:1.0"
+nginx_image="test-nginx-proxy:1.0"
+database_ip="172.31.1.100"
 
-### MYSQL ###
+### DATABASE-MYSQL ###
 if docker ps -a | grep -q "docker-db"; then
     docker stop docker-db
     docker rm docker-db
@@ -13,8 +16,9 @@ fi
 
 docker run -itd --name docker-db \
 --restart=always \
--p 3306:3306
-${database-image}
+--network=${back_net} \
+--ip ${database_ip} \
+${database_image}
 
 ### BACKEND-FLASK ###
 if docker ps -a | grep -q "docker-backend"; then
@@ -27,19 +31,23 @@ if docker ps -a | grep -q "docker-backend"; then
 fi
 
 docker run -itd --name docker-backend-1 \
---restart=always
--p 5001:5000
-${backend-image}
+--restart=always \
+--network=${back_net} \
+-e MYSQL_HOST=${database_ip} \
+${backend_image}
+# -p 5001:5000 \
 
 docker run -itd --name docker-backend-2 \
---restart=always
--p 5002:5000
-${backend-image}
+--restart=always \
+--network=${back_net} \
+${backend_image}
+# -p 5001:5000 \
 
 docker run -itd --name docker-backend-3 \
---restart=always
--p 5003:5000
-${backend-image}
+--restart=always \
+--network=${back_net} \
+${backend_image}
+# -p 5001:5000 \
 
 ### HAPROXY ###
 if docker ps -a | grep -q "docker-haproxy"; then
@@ -48,9 +56,11 @@ if docker ps -a | grep -q "docker-haproxy"; then
 fi
 
 docker run -itd --name docker-haproxy \
---restart=always
--p 8080:8080
-${haproxy-image}
+--restart=always \
+--network=${front_net} \
+--network=${back_net} \
+-p 8080:8080 \
+${haproxy_image}
 
 
 ### FRONTEND-NEXTJS ###
@@ -64,19 +74,22 @@ if docker ps -a | grep -q "docker-frontend"; then
 fi
 
 docker run -itd --name docker-frontend-1 \
---restart=always
--p 3001:80
-${frontend-image}
+--restart=always \
+--network=${front_net} \
+${frontend_image}
+# -p 3001:80 \
 
 docker run -itd --name docker-frontend-2 \
---restart=always
--p 3002:80
-${frontend-image}
+--restart=always \
+--network=${front_net} \
+${frontend_image}
+# -p 3002:80
 
 docker run -itd --name docker-frontend-3 \
---restart=always
--p 3003:80
-${frontend-image}
+--restart=always \
+--network=${front_net} \
+${frontend_image}
+# -p 3003:80
 
 ### NGINX ###
 if docker ps -a | grep -q "docker-nginx"; then
@@ -85,6 +98,7 @@ if docker ps -a | grep -q "docker-nginx"; then
 fi
 
 docker run -itd --name docker-nginx \
---restart=always
--p 80:80
-${nginx-image}
+--restart=always \
+--network=${front_net} \
+-p 80:80 \
+${nginx_image}
